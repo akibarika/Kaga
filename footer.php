c43653dc0ba7bdcc5eef54bab14a355a78280ead
Rika <lxclxc89816@gmail.com>
2014-06-25 19:25:48 +1200

    Updated v0.02

---
+++ footer.php
@@ -0,0 +1,4 @@
+
+        <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/cNav.js"></script>
+    </body>
+</html>
---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Akiba
+ * Date: 14-6-25
+ * Time: 下午6:30
+ */
+
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 9999, 576, true);
