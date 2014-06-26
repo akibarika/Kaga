863c31919c57222a17aaca38eb74f9ae538a5320
Rika <lxclxc89816@gmail.com>
2014-06-27 02:41:21 +1200

    Updated v0.05

--- content.php
+++ content.php
@@ -14,15 +14,20 @@
     </a>
     <header class="entry-header">
         <div class="entry-meta">
-                                <span class="cat-links">
-                                    <?php the_category(' '); ?>
-                                </span>
+            <span class="cat-links">
+                <?php the_category(' '); ?>
+            </span>
         </div>
         <h1 class="entry-title">
             <a rel="bookmark" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
         </h1>
         <div class="entry-meta">
-
+            <span class="entry-date">
+                <a rel="bookmark" href="<?php the_permalink() ?>"><time class="fa fa-clock-o time-link"><?php the_time('F j, Y'); ?></time></a>
+            </span>
+            <span class="comments-link">
+                <?php comments_popup_link( 'No comments yet', '1 comment', '% comments', 'fa fa-comment-o  comment-link', 'Comments are off for this post'); ?>
+            </span>
         </div>
     </header>
     <div class="entry-content">
